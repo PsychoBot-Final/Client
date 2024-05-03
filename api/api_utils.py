331c4e5a9b6328c2a32e0b93642e2d3f926aa073
Motheen <50198896+motheen-b@users.noreply.github.com>
2024-05-03 16:36:22 -0400

    - Added API_UTILS

---
+++ api/api_utils.py
@@ -0,0 +1,22 @@
+import os
+import cv2 as cv
+from constants import TEMPLATES_DIR_PATH, VALID_TEMPLATE_EXTENSIONS
+
+
+def load_templates(dir_name: str) -> dict:
+    """
+    The function `load_templates` loads image templates from a specified directory and stores them in a
+    dictionary.
+    
+    Args:
+      dir_name (str): The `dir_name` parameter is a string that represents the name of the directory
+    where the templates are located.
+    """
+    templates = {}
+    templates_dir = os.path.join(TEMPLATES_DIR_PATH, dir_name)
+    for template in os.listdir(templates_dir):
+        name, ext = os.path.splitext(template)
+        if ext.lower() in VALID_TEMPLATE_EXTENSIONS:
+            template_path = os.path.join(templates_dir, template)
+            if not name in templates:
+                templates[name] = cv.imread(template_path, cv.IMREAD_UNCHANGED)
