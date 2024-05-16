47aa5b54f0c2a705632d27d7944ef0905be35cbb
Motheen <50198896+motheen-b@users.noreply.github.com>
2024-05-16 04:09:03 -0400

    - Delete existing api template folders before extracting new ones

--- scripts/templates/templates.py
+++ scripts/templates/templates.py
@@ -9,16 +9,19 @@ def templates_dir_exists(file_name: str) -> bool:
     return os.path.isdir(os.path.join(TEMPLATES_DIR_PATH, file_name))
 
 def unzip_templates(file_name: str, file_data: bytes) -> str:
+    file_name = file_name.replace('.zip', '') if file_name.endswith('.zip') else file_name
     fd, tmp_file_path = tempfile.mkstemp()
     os.close(fd)
-    extracted_path = None
+    dir_path = os.path.join(TEMPLATES_DIR_PATH, file_name)
     try:
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+
         with open(tmp_file_path, 'wb') as tmp_file:
             tmp_file.write(file_data)
 
         with zipfile.ZipFile(tmp_file_path, 'r') as zip_ref:
             zip_ref.extractall(TEMPLATES_DIR_PATH)
-            extracted_path = os.path.join(TEMPLATES_DIR_PATH, file_name)
     except zipfile.BadZipFile:
         print("Failed to unzip: Corrupted zip file.")
         return None
@@ -30,7 +33,7 @@ def unzip_templates(file_name: str, file_data: bytes) -> str:
         return None
     finally:
         os.unlink(tmp_file_path)
-    return extracted_path
+    return dir_path
 
 def remove_templates_dir(dir_name: str) -> None:
     try:
